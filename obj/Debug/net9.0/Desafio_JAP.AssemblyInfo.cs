a11622d37471acde6b89f3759fd96a1cc0751088
miguel33177 <bruno_bessa02@hotmail.com>
2025-01-04 00:54:08 +0000

    css navbar

--- obj/Debug/net9.0/Desafio_JAP.AssemblyInfo.cs
+++ obj/Debug/net9.0/Desafio_JAP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio_JAP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9233ff4a53ca365dfbe04451c9f2bddf8c838bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3840c2c8bd016e627950037a33729822c5a70d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio_JAP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio_JAP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
